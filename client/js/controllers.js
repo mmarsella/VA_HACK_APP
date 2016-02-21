ae42b231c5cfd07659872ade8488ec81d53502d7
Mark Marsella <mmarsella114@g.rwu.edu>
2016-02-21 14:00:04 -0800

    styling, navbar, final changes before merging

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,15 +2,56 @@
 // USER DASHBOARD
 app.controller("MainController", function($scope,$auth,$compile,$timeout, uiCalendarConfig){
    $scope.currentUser = $auth.getPayload().user;
-   $scope.jobs = [{name:"Google",position:"Software Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"},
-   {name:"Ebay",position:"Front End Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"},
-   {name:"Facebook",position:"Backend Software Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"}]
-   $scope.details = true;
+   // $scope.jobs = [{name:"Google",position:"Software Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"},
+   // {name:"Ebay",position:"Front End Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"},
+   // {name:"Facebook",position:"Backend Software Engineer",description:"Searching for someone with experience in NodeJS, AngularJS, JavaScript and SQL.",experience:"5 Years",education:"BA Computer Science"}]
+   // $scope.details = true;
    $scope.showDetails = function(){
     $scope.details = $scope.details ? false : true;
     }
-   
 
+    $scope.formVisible = false;
+  
+    $scope.jobs = [
+      {
+        name: "Cattle Empire, LLC",
+        position: "Staff Accountant",
+        description: "This position reports financial performance, performs various accounting and banking duties, and provides financial expertise to others in the company.",
+        image: "https://pbs.twimg.com/profile_images/3354297056/4cf064bd5891504e21d888fea0bcaaf8_400x400.png",
+        postRating: 0,
+        category: "Business",
+        salary: "$60k-80k"
+        // job_title: "Health Care"
+      },  
+      {
+        name: "Endsight",
+        position: "Help Desk Engineer",
+        description: "A Help Desk Engineer provides support to Endsight’s clients on all supported networks, platforms, and applications. Generally, the person in this role troubleshoots and then determines the source of the computer problems, and advises/implements appropriate action/solutions.",
+        image: "https://pbs.twimg.com/profile_images/1174150167/Arrow_Bug_reversed.jpg",
+        postRating: 0,
+        category: "Information Technology",
+        salary: "$40k-50k"
+
+      },
+      {
+        name: "Polyclinic",
+        position: "IT Help Desk Analyst",
+        description: "The IT Help Desk Analyst’s primary role is to act as a first-level problem identification and resolution resource for physicians, administrators, patients, and other Polyclinic employees.  This includes: answering questions, providing advice, troubleshooting, coordinating resolution with other IT groups, and following-up with users to help them solve their IT and EHR problems.",
+        image: "https://www.nhgp.com.sg/uploadedImages/About_Us/V_NHGP_logo_RGB(1).png",
+        postRating: 0,
+        category: "Information Technology",
+        salary: "$60k-80k"
+      },
+      {
+        name: "Critical Mass",
+        position: "Technical Support Analyst",
+        description: "The Technical Support Analyst provides support covering all aspects of Critical Mass’ desktop computing environment. This includes providing support from building new systems to supporting the end user on PC or Mac platforms. This role requires a motivated, personable individual willing to learn all aspects of supporting our dynamic environment in a team setting. The Technical Support Analyst is part of a team that applies the latest Internet technologies in creating world class on-line experiences for our clients.",
+        image: "https://media.glassdoor.com/lst/15389/critical-mass-office.jpg",
+        postRating: 0,
+        category: "Business",
+        salary: "$60k-80k"
+      }    
+    ];
 });  // END CONTROLLER
 
 app.controller("LoginController", function($scope, $auth, $location, UserService){
@@ -60,7 +101,9 @@ app.controller('LogoutController', function($location, $auth) {
       });
   });
 
-app.controller('ProfileController', function($location, $auth) {
+app.controller('ProfileController', function($scope, $location, $auth) {
+     $scope.currentUser = $auth.getPayload().user;
+
 
   });
 
